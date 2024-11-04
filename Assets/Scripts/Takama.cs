097c2958b923c8ffa93d24f3db13384fb2f7f231
uldynia <85220330+uldynia@users.noreply.github.com>
2024-11-04 16:21:31 +0800

    draw line

--- Assets/Scripts/Takama.cs
+++ Assets/Scripts/Takama.cs
@@ -26,6 +26,7 @@ public class Takama : NetworkBehaviour // the main game. Izumo is the lobby.
             yield return new WaitForSeconds(1);
             BoxFill(Vector3Int.zero, 1, 0, 0, xsize, ysize);
             GenerateCircle(spawnpoint.x, spawnpoint.y, 30);
+            LineFill(0, (Vector3Int)spawnpoint, (Vector3Int)(spawnpoint + new Vector2Int(100, -100)), 3);
             TeleportToSpawn();
         }
     }
@@ -51,6 +52,39 @@ public class Takama : NetworkBehaviour // the main game. Izumo is the lobby.
     {
         //Debug.Log($"Filling box {startX} {startY} to {endX} {endY}");
         tilemap.BoxFill(position, tiles[id], startX, startY, endX, endY);
+    }
+    [ClientRpc]
+    public void LineFill(int id, Vector3Int start, Vector3Int end, int thickness)
+    {
+        List<Vector3Int> vectors = new();
+        List<TileBase> _tiles = new();
+
+        int dx = Mathf.Abs(end.x - start.x);
+        int dy = Mathf.Abs(end.y - start.y);
+        int sx = start.x < end.x ? 1 : -1;
+        int sy = start.y < end.y ? 1 : -1;
+        int err = dx - dy;
+
+        while (true)
+
+        {
+            for (int x = -thickness / 2; x <= thickness / 2; x++)
+            {
+                for (int y = -thickness / 2; y <= thickness / 2; y++)
+                {
+                    vectors.Add(new Vector3Int(start.x + x, start.y + y, start.z));
+                    _tiles.Add(tiles[id]);
+                }
+            }
+
+            if (start.x == end.x && start.y == end.y)
+                break;
+            int e2 = 2 * err;
+            if (e2 > -dy) { err -= dy; start.x += sx; }
+            if (e2 < dx) { err += dx; start.y += sy; }
+        }
+
+        tilemap.SetTiles(vectors.ToArray(), _tiles.ToArray());
     }
     [ClientRpc]
     public void SetTile(Vector3Int position, int id)
