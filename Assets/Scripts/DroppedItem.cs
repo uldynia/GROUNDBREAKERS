05354251f9517be463362a6e8978051051b60c5f
uldynia <uldynia@gmail.com>
2024-12-12 19:30:43 +0800

    dropped item animation

--- Assets/Scripts/DroppedItem.cs
+++ Assets/Scripts/DroppedItem.cs
@@ -1,3 +1,4 @@
+using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,11 @@ public class DroppedItem : NetworkBehaviour
     [Server]
     public void Init(Item item) {
         image.sprite = item.sprite;
+    }
+    float time;
+    public void Update() {
+        transform.localScale += (Vector3.one * 0.8f - transform.localScale) * Time.deltaTime * 5;
+        transform.position += Vector3.up * MathF.Sin((time +=Time.deltaTime) * 2) * 0.002f;
     }
     [Server]
     void OnTriggerEnter2D(Collider2D other) {
