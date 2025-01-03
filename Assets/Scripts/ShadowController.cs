b8ab4d45a3adb8905656c7df72e6767d2f61601a
uldynia <uldynia@gmail.com>
2025-01-03 10:13:54 +0800

    enemy AI base

--- Assets/Scripts/ShadowController.cs
+++ Assets/Scripts/ShadowController.cs
@@ -9,19 +9,60 @@ public class ShadowController : NetworkBehaviour
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
-        if(isServer) {
+        if (isServer)
+        {
             e.onDeath += OnDeath;
         }
     }
-    void OnDeath(float damage) {
+    void OnDeath(float damage)
+    {
         NetworkServer.Destroy(gameObject);
     }
-    // Update is called once per frame
+
+    public enum STATE
+    {
+        IDLE,
+        PATROL,
+        CHASE
+    }
+    public STATE state;
+    float stateDuration, direction, jumpCooldown;
     void Update()
     {
-        sr.sprite = walkingSpriteSheet[(int)( (Time.time * 3) % walkingSpriteSheet.Length)];
-        if(isServer) {
-            
+        if (!isServer) return;
+        stateDuration += Time.deltaTime;
+        jumpCooldown += Time.deltaTime;
+        switch (state)
+        {
+            case STATE.IDLE:
+                sr.sprite = walkingSpriteSheet[0];
+                if (stateDuration > 1)
+                {
+                    state = STATE.PATROL;
+                    stateDuration = 0;
+                    direction = Random.Range(-1, 1) == -1 ? -1 : 1;
+                }
+                break;
+            case STATE.PATROL:
+                sr.sprite = walkingSpriteSheet[(int)((Time.time * 3) % walkingSpriteSheet.Length)];
+                var diff = new Vector3(direction, 0, 0);
+
+                if (jumpCooldown > 2 && Physics2D.Raycast(transform.position, diff, 3, LayerMask.NameToLayer("Terrain")))
+                {
+                    jumpCooldown = 0;
+                    GetComponent<Rigidbody2D>().AddForce(Vector3.up * 5);
+                }
+                transform.position += diff * Time.deltaTime * 4;
+
+                if (stateDuration > 1.5f)
+                {
+                    state = STATE.IDLE;
+                    stateDuration = 0;
+                }
+                break;
+            case STATE.CHASE:
+
+                break;
         }
     }
 }
