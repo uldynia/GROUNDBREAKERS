112e4a39465d8765ef5c9786a770ad37a5785dbb
uldynia <uldynia@gmail.com>
2025-02-02 17:24:35 +0800

    grappling hook visuals fixed

--- Assets/Scripts/PowerTools/GrapplingHook/GrapplingHook.cs
+++ Assets/Scripts/PowerTools/GrapplingHook/GrapplingHook.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GrapplingHook : PowerTool
 {
@@ -10,24 +12,34 @@ public class GrapplingHook : PowerTool
         public Vector2 point;
         public float duration = 2;
     }
-    public List<Point> points = new();
+    public SyncList<Point> points = new();
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+    }
+    void OnEnable()
+    {
+        RenderPipelineManager.endCameraRendering += RenderLines;
+    }
+
+    void OnDisable()
+    {
+        RenderPipelineManager.endCameraRendering -= RenderLines;
     }
     public override void OnRelease()
     {
+        if (!isOwned) return;
         base.OnRelease();
-
         var hit = Physics2D.Raycast(transform.position, button.deltaNormalized, Mathf.Infinity, LayerMask.GetMask("Terrain"));
         if (hit.collider == null) return;
         Point point = new();
-        point.point = (hit.point - rb.position);
+        point.point = hit.point;
         points.Add(point);
     }
     private void Update()
     {
-        if (points.Count == 0) return;
+        if (!isOwned || points.Count == 0) return;
         rb.linearVelocity = Vector3.zero;
         Vector2 finalVector = new();
         for (int i = 0; i < points.Count; i++)
@@ -42,9 +54,32 @@ public class GrapplingHook : PowerTool
             }
         }
         finalVector /= points.Count;
-        rb.position += finalVector.normalized * Time.deltaTime * 10;
+        var diff = (finalVector - rb.position);
+        rb.position += diff.normalized * Time.deltaTime * 10;
+        if(diff.sqrMagnitude < 1) points.Clear();
 
     }
 
+    public Material mat;
+    void RenderLines(ScriptableRenderContext context, Camera camera)
+    {
+        GL.PushMatrix();
+        GL.LoadOrtho();
+        if (mat.SetPass(0))
+        {
+            Vector2 playerpos = camera.WorldToViewportPoint(rb.position);
+            foreach (var point in points)
+            {
+                Vector2 grapplePos = camera.WorldToViewportPoint(point.point);
+                GL.Begin(GL.LINES);
+                GL.Color(Color.red);
+                GL.Vertex3(playerpos.x, playerpos.y, 0);
+                GL.Vertex3(grapplePos.x, grapplePos.y, 0);
+                GL.End();
+            }
 
+        }
+        GL.PopMatrix();
+
+    }
 }
