70a572688af41f7d054c679bb1ace21a3d482d3b
uldynia <uldynia@gmail.com>
2025-02-08 19:25:02 +0800

    player dead base

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : NetworkBehaviour
     public static PlayerController instance;
     public static string menuUsername;
     [SyncVar] public string username = "user";
+
+    [SyncVar] public int sprite = 0;
+    [SerializeField] Sprite[] spritesheet;
+
     public Rigidbody2D rb;
     SpriteRenderer sr;
     bool canJump = true;
     public float cameraTargetSize = 10;
+    Entity e;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        e = GetComponent<Entity>();
         Application.targetFrameRate = 120;
         ControlsManager.instance.jumpButton.onDown += Jump;
         if (isLocalPlayer)
@@ -33,21 +39,36 @@ public class PlayerController : NetworkBehaviour
     void Update()
     {
         sr.flipX = isFlipped;
+        sr.sprite = spritesheet[sprite];
         if (!isOwned) return;
-        if (ControlsManager.instance.horizontal != 0)
-            isFlipped = (ControlsManager.instance.horizontal > 0);
-        if ((canJump))
+        if (e.health > 0)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (ControlsManager.instance.horizontal != 0)
             {
-                Jump();
+                isFlipped = (ControlsManager.instance.horizontal > 0);
+                sprite = Mathf.RoundToInt(Time.time * 5) % 2;
             }
+            else
+            {
+                sprite = 0;
+            }
+            if (canJump)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Jump();
+                }
+                transform.position += new Vector3(ControlsManager.instance.horizontal * Time.deltaTime * 10, 0);
 
-            transform.position += new Vector3(ControlsManager.instance.horizontal * Time.deltaTime * 10, 0);
-
+            }
+            if (Vector3.SqrMagnitude(Camera.main.transform.position - transform.position) > 20) Camera.main.transform.position = transform.position - new Vector3(0, 0, 10);
+            Camera.main.transform.position += ((transform.position - new Vector3(0, 0, 10) - Camera.main.transform.position)) * Time.fixedDeltaTime * 5;
         }
-        if (Vector3.SqrMagnitude(Camera.main.transform.position - transform.position) > 20) Camera.main.transform.position = transform.position - new Vector3(0, 0, 10);
-        Camera.main.transform.position += ((transform.position - new Vector3(0, 0, 10) - Camera.main.transform.position)) * Time.fixedDeltaTime * 5;
+        else
+        {
+            sprite = 2;
+        }
+
         Camera.main.orthographicSize += Mathf.Clamp((cameraTargetSize - Camera.main.orthographicSize), -0.5f, 0.5f) * Time.deltaTime * 10;
     }
     void FixedUpdate()
